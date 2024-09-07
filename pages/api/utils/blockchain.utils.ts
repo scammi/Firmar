e2787a72d42f1c41273d4afa94450e1f76d42215
scammi <scammi@gmail.com>
2024-09-07 16:55:15 -0300

    clear order on chain utils

--- pages/api/utils/blockchain.utils.ts
+++ pages/api/utils/blockchain.utils.ts
@@ -44,44 +44,6 @@ export const lockMintContractConfig = {
   abi: lockMintAbi
 };
 
-// Function to call lockMint
-export async function callLockMint(to: Address, uri: string) {
-    try {
-      // Simulate the transaction
-      const { request, result } = await avaxPublicClient.simulateContract({
-        ...lockMintContractConfig,
-        functionName: 'lockMint',
-        args: [to, uri],
-        account: account,
-      })
-      debugger;
-      // Write the contract
-      const hash = await walletClient.writeContract(request);
-  
-      console.log('Transaction sent:', hash);
-  
-      // Wait for the transaction to be mined
-      const receipt = await avaxPublicClient.waitForTransactionReceipt({ hash });
-      console.log('Transaction mined:', receipt.transactionHash);
-  
-      // Check if the transaction was successful
-      if (receipt.status === 'success') {
-        console.log('Transaction successful');
-        
-        if (result > 0) {
-          return { hash: receipt.transactionHash, tokenId: result };
-        } else {
-          console.warn('Unable to extract tokenId from transaction logs');
-          return { hash: receipt.transactionHash, tokenId: null };
-        }
-      } else {
-        throw new Error('Transaction failed');
-      }
-    } catch (error) {
-      console.error('Error in callLockMint:', error);
-      throw error;
-    }
-}
   
 export async function createCertifiedSignerAttestation(
   to: Address,
@@ -155,4 +117,44 @@ export async function createCertifiedSignerAttestation(
   console.log('Transaction sent:', hash)
 
   return { attestationId: 1, txHash: hash}
+}
+
+
+// Function to call lockMint
+export async function callLockMint(to: Address, uri: string) {
+    try {
+      // Simulate the transaction
+      const { request, result } = await avaxPublicClient.simulateContract({
+        ...lockMintContractConfig,
+        functionName: 'lockMint',
+        args: [to, uri],
+        account: account,
+      })
+      debugger;
+      // Write the contract
+      const hash = await walletClient.writeContract(request);
+  
+      console.log('Transaction sent:', hash);
+  
+      // Wait for the transaction to be mined
+      const receipt = await avaxPublicClient.waitForTransactionReceipt({ hash });
+      console.log('Transaction mined:', receipt.transactionHash);
+  
+      // Check if the transaction was successful
+      if (receipt.status === 'success') {
+        console.log('Transaction successful');
+        
+        if (result > 0) {
+          return { hash: receipt.transactionHash, tokenId: result };
+        } else {
+          console.warn('Unable to extract tokenId from transaction logs');
+          return { hash: receipt.transactionHash, tokenId: null };
+        }
+      } else {
+        throw new Error('Transaction failed');
+      }
+    } catch (error) {
+      console.error('Error in callLockMint:', error);
+      throw error;
+    }
 }
