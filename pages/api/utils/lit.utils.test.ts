14a907df84c3eae3568b1af674bc8a7cf9498b93
scammi <scammi@gmail.com>
2024-09-07 11:49:32 -0300

    expect

--- pages/api/utils/lit.utils.test.ts
+++ pages/api/utils/lit.utils.test.ts
@@ -1,9 +1,12 @@
 import { signMessage } from "viem/actions";
 import { walletClient } from "./lit.utils";
+import { expect, describe, it } from '@jest/globals';
 
 describe('Lit protocol', () => {
   it ('Runs tets', async () => {
     const signature = await signMessage(walletClient, { message: ': )' });
+    expect(typeof signature).toBe('string');
+
     console.log(signature);
   });
 });
